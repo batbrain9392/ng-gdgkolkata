002fdc1792ddb062c8340fc7ce184c289e44a865
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-06-21 20:24:29 +0530

    other routes disabled for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './core/landing/landing.component';
+// import { LandingComponent } from './core/landing/landing.component';
 import { PathNotFoundComponent } from './core/path-not-found/path-not-found.component';
-import { SigninComponent } from './core/auth/signin/signin.component';
-import { SignupComponent } from './core/auth/signup/signup.component';
+// import { SigninComponent } from './core/auth/signin/signin.component';
+// import { SignupComponent } from './core/auth/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
+  // { path: 'signin', component: SigninComponent },
+  // { path: 'signup', component: SignupComponent },
   {
     path: 'feature',
     loadChildren: () =>
       import('./feature/feature.module').then(m => m.FeatureModule)
   },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then(m => m.AdminModule)
-  },
-  { path: '', component: LandingComponent },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () =>
+  //     import('./admin/admin.module').then(m => m.AdminModule)
+  // },
+  // { path: '', component: LandingComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/feature' },
   { path: '**', component: PathNotFoundComponent }
 ];
 
